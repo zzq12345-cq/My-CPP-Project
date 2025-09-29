8220988c545f0a781ae2f5a7c9c065f91340544c
zhouzhiqi <2477664538@qq.com>
2025-09-29 19:35:25 +0800

    feat: 添加计算绝对值的功能

---
+++ product01/绝对值.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    double m;
+    cin >> m;
+    if (m > 0){
+        cout << m;
+    }else{
+        cout << -m;
+    }
+    return 0;
+}
